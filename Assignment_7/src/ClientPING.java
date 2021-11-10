c353776a45ef27577aa4fd5efc160a5eb5b906cd
Gianma0910 <60882454+Gianma0910@users.noreply.github.com>
2021-11-10 18:32:31 +0100

    JAVA PINGER single thread

--- Assignment_7/src/ClientPING.java
+++ Assignment_7/src/ClientPING.java
@@ -6,127 +6,129 @@ import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.sql.Timestamp;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class ClientPING {
-	
-	static int port;
-	static String nameServer;
-	static final int receiveTimeout = 2000;
-	
+
+	private static int portServer;
+	private static String nameServer;
+	private static int receiveTimeout = 2000;
+
 	public static void main(String[] args) throws UnknownHostException {
-		if(args.length < 2) {
-			System.err.println("Numero di argomenti errato\n");
-			System.err.println("Inserire nome e porta del server\n");
-		
+		if(args.length != 2) {
+			System.err.println("Numero di argomenti inseriti errato\n");
+			System.err.println("Inserire il nome e la porta del server\n");
 			return;
-		}else {
-			port = Integer.parseInt(args[0]);
-			nameServer = args[1];
-			
-			if(port != ServerPING.port) {
-				System.err.println("ERR -arg 1");
-				return;
-			}else if(nameServer != ServerPING.name) {
-				System.err.println("ERR -arg 2");
-				return;
-			}else if(port != ServerPING.port && nameServer != ServerPING.name) {
-				System.err.println("ERR -arg 1");
-				System.err.println("ERR -arg 2");
-				return;
-			}
+		}
+	
+		nameServer = args[0];
+		portServer = Integer.parseInt(args[1]);
+		InetAddress address;
+		DatagramSocket socket; 
+		ArrayList<Boolean> packetRefused = new ArrayList<>(Arrays.asList(new Boolean[10]));
+		int packetReceived = 0;
+		int seqNumber = 0;
+		byte [] response;
+		long [] RTTPacket = new long[10];
+
+		Collections.fill(packetRefused, Boolean.FALSE);
+		
+		try {
+			address = InetAddress.getByName(nameServer);
+			socket = new DatagramSocket(0);
+		} catch (SocketException e) {
+			System.out.println("ERR -arg 2\n");
+			return;
+		}catch(UnknownHostException e) {
+			System.err.println("ERR -arg 1\n");
+			return;
 		}
 		
-		InetAddress serverAddress = InetAddress.getByName(nameServer);
-		int packetReceived = 0;
-		long [] RTTPacket = new long[10]; 
-		
-		
-		try(DatagramSocket socket = new DatagramSocket(port, serverAddress)){
-			for(Integer i = 0; i < 10; i++) {
-				String seqNumber = i.toString();
-				Timestamp timePacketSend = new Timestamp(System.currentTimeMillis());
-				String time = timePacketSend.toString();
-				String message = seqNumber + " " + time;
-				byte[] response = new byte[message.length()];		
-				DatagramPacket requestPING = new DatagramPacket(message.getBytes(), message.length(), serverAddress, port);
+		try {
+			while(seqNumber < 10){
+				Timestamp timeSend = new Timestamp(System.currentTimeMillis());
+				String messagePING = "PING " + seqNumber + " " + timeSend;
+				DatagramPacket requestPING = new DatagramPacket(messagePING.getBytes(), messagePING.length(), address, portServer);
 				socket.send(requestPING);
-				DatagramPacket responsePING = new DatagramPacket(response, response.length);
-				
+				int temp = seqNumber;
+				seqNumber++;
+				response = new byte[messagePING.length()];
+				DatagramPacket responsePING = new DatagramPacket(response, response.length); 	
+			
 				try {
 					socket.setSoTimeout(receiveTimeout);
 					socket.receive(responsePING);
+					Timestamp timeReceived = new Timestamp(System.currentTimeMillis());
+					RTTPacket[temp] = timeReceived.getTime() - timeSend.getTime();
+					String msg = new String(response, 0, responsePING.getLength());
+					
+					if(packetRefused.get(temp).equals(Boolean.FALSE)) {
+						packetReceived++;
+						System.out.println(msg + " RTT (ms) : " + RTTPacket[temp]);
+						System.out.println("\n");
+					}
 				}catch(SocketTimeoutException e) {
 					System.out.println("*\n");
+					packetRefused.set(temp, Boolean.TRUE);
+					RTTPacket[temp] = -1;
 				}
-				
-				Timestamp timePacketReceive = new Timestamp(System.currentTimeMillis());
-				System.out.println(responsePING.getData().toString() + "\n");
-				packetReceived++;
-				RTTPacket[i] = timePacketSend.getTime() - timePacketReceive.getTime();
-				
+			
 			}
-			
-			int percentagePacketLost = (10 - packetReceived)*10;
-			float minRTT = minRTT(RTTPacket);
-			float avgRTT = avgRTT(RTTPacket);
-			float maxRTT = maxRTT(RTTPacket);
-
-//			---- PING Statistics ----
-//
-//			10 packets transmitted, 7 packets received, 30% packet loss, round-trip (ms) min/avg/max = 63/190.29/290
-			
-			System.out.println("---- PING Statistics ----");
-			System.out.printf("10 packets transmitted, %d" + packetReceived + " packets received, %d"
-					+ percentagePacketLost + "% packet loss, round-trim (ms) min/avg/max = %f"
-					+ minRTT + "/%.2f" + avgRTT + "/%f" + maxRTT);
-
-			
-		}catch(SocketException e) {
-			System.err.println("Errore nella creazione del socket\n");
-			e.printStackTrace();
-		}catch(UnknownHostException e) {
-			System.err.println("Host non ricosciuto\n");
-			e.printStackTrace();
 		}catch(IOException e) {
-			System.err.println("Errore di I/O, impossibile mandare o ricevere messaggi\n");
-			e.printStackTrace();
+			System.err.println("ERR -arg 2\n");
+			socket.close();
+			return;
 		}
 		
+		int percentagePacketLoss = (10 - packetReceived) * 10;
+		long minRTT = minRTT(RTTPacket);
+		long maxRTT = maxRTT(RTTPacket);
+		float avgRTT = avgRTT(RTTPacket);
+		DecimalFormat df = new DecimalFormat();
+		df.setMaximumFractionDigits(2);
 		
-	}
-
-	private static float maxRTT(long[] RTTPacket) {
-		float max = RTTPacket[0];
+		System.out.println("---- PING Statistics ----");
+		System.out.println("10 packets transmitted, " + packetReceived + " packets received, " + percentagePacketLoss + "% packet loss"
+							+ "\n" + "round-trip (ms) min/avg/max " + minRTT + "/" + df.format(avgRTT) + "/" + maxRTT + "\n");
 		
-		for(int i = 1; i < 10; i++) {
-			if(RTTPacket[i] > max)
-				max = RTTPacket[i];
-		}
-		
-		return max;	
+		socket.close();
 	}
 
 	private static float avgRTT(long[] RTTPacket) {
 		float avg = 0;
-		float sum = 0;
-		
+		long sum = 0;
+				
 		for(int i = 0; i < 10; i++) {
 			sum += RTTPacket[i];
-		}
+		}		
 		
 		avg = sum / 10;
+		
 		return avg;
 	}
 
-	private static float minRTT(long[] RTTPacket) {
-		float min = RTTPacket[0];
+	private static long maxRTT(long[] RTTPacket) {
+		long max = RTTPacket[0];
 		
 		for(int i = 1; i < 10; i++) {
-			if(RTTPacket[i] < min)
+			if(max < RTTPacket[i])
+				max = RTTPacket[i];
+		}
+		
+		return max;
+	}
+
+	private static long minRTT(long[] RTTPacket) {
+		long min = RTTPacket[0];
+		
+		for(int i = 1; i < 10; i++) {
+			if(min > RTTPacket[i])
 				min = RTTPacket[i];
 		}
 		
 		return min;
 	}
-
 }
--- Assignment_7/src/ServerPING.java
+++ Assignment_7/src/ServerPING.java
@@ -2,75 +2,63 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ServerPING {
 
-	static final String name = "localhost";
 	static int port;
-	static long seed;
+	static int seed;
 	
-	public static void main(String[] args) {
-		if(args.length < 2) {
-			System.err.println("Numero di argomenti errato\n");
-			System.err.println("Inserire il numero di porta e un seed per la generazione casuale della latenza e della perdita dei pacchetti\n");
+	public static void main(String[] args) throws InterruptedException {
+		if(args.length != 2) {
+			System.err.println("Numero di argomenti inseriti errato\n");
+			System.err.println("Inserire la porta del server e il seed\n");
 			return;
-		}else {
-			port = Integer.parseInt(args[0]);
-			seed = Integer.parseInt(args[1]);
-			
-			if(port < 0 || (port >= 0 && port <= 1024)) {
-				System.err.println("ERR -arg 1");
-				return;
-			}else if(seed <= 0) {
-				System.err.println("ERR -arg 2");
-				return;
-			}else if(port < 0 || (port >= 0 && port <= 1024) && seed <= 0) {
-				System.err.println("ERR -arg 1");
-				System.err.println("ERR -arg 2");
-				return;
-			}
 		}
 		
-		byte [] requestAndResponse = new byte[30];
+		port = Integer.parseInt(args[0]);
+		seed = Integer.parseInt(args[1]);
 		
-		try(DatagramSocket socket = new DatagramSocket(port)) {
-			for(Integer i = 0; i < 10; i++) {
-				long latency = ThreadLocalRandom.current().nextLong(seed);
-				long percentagePacketLoss = ThreadLocalRandom.current().nextLong(0, seed);
-				DatagramPacket requestPING = new DatagramPacket(requestAndResponse, requestAndResponse.length);
+		if(seed <= 0) {
+			System.err.println("ERR -arg 2\n");
+			return;
+		}
+		
+		byte [] request = new byte[30];
+		
+		try{
+			DatagramSocket socket = new DatagramSocket(port);
+			System.out.println("Server is running...\n");
+			int i = 0;
+			while(i < 10) {
+				int latency = ThreadLocalRandom.current().nextInt(100, 3000);
+				int percentagePacketLoss = ThreadLocalRandom.current().nextInt(1, 101); 
+				DatagramPacket requestPING = new DatagramPacket(request, request.length);
 				socket.receive(requestPING);
-				
-				if(percentagePacketLoss <= ((seed*100)/25)) {
-					System.out.println("Indirizzo IP e porta del Client: " + requestPING.getAddress() + "; " + requestPING.getPort()
-										+ "\n" + "Messaggio di PING: " + requestPING.getData().toString() + "\n"
-										+ "Azione server: PING non inviato");
+
+				if(percentagePacketLoss >= 25) {
+					DatagramPacket responsePING = new DatagramPacket(request, request.length, requestPING.getAddress(), requestPING.getPort());
+					Thread.sleep(latency);
+					socket.send(responsePING);
+					String msg = new String(request, 0, responsePING.getLength());
+					System.out.println("Indirizzo ip e porta del client : " + requestPING.getAddress() + "; " + requestPING.getPort() + "\n"
+										+ msg + "\n" + "PING ritardato di : " + latency + "(ms)\n");
+					i++;
 				}else {
-					DatagramPacket responsePING = new DatagramPacket(requestAndResponse, requestAndResponse.length, requestPING.getAddress(), requestPING.getPort());
-					try {
-						Thread.sleep(latency);
-					}catch(InterruptedException e) {
-						System.err.println("Thread interrotto prima della fine dello sleep\n");
-						e.printStackTrace();
-					}
-					socket.send(responsePING);	
-					System.out.println("Indirizzo IP e porta del Client: " + requestPING.getAddress() + "; " + requestPING.getPort()
-										+ "\n" + "Messaggio di PING: " + requestPING.getData().toString() + "\n"
-										+ "Azione server: PING ritardato di " + latency + " ms\n");
+					String msg = new String(request, 0, requestPING.getLength());
+					System.out.println("Indirizzo ip e porta del client : " + requestPING.getAddress() + "; " + requestPING.getPort() + "\n"
+							+ msg + "\n" + "PING non inviato\n");
+					i++;
 				}
 			}
-		}catch(SocketException e) {
-			System.err.println("Errore nella creazione del socket\n");
+			
+			socket.close();
+		} catch (SocketException e) {
+			System.out.println("ERR -arg 1\n");
 			return;
-		}catch(UnknownHostException e) {
-			System.err.println("Host non riconosciuto\n");
-			return;
-		}catch(IOException e) {
-			System.err.println("Errore di I/O, impossibile mandare e ricevere messaggi\n");
+		} catch (IOException e) {
+			System.err.println("ERR -arg 1\n");
 			return;
 		}
 	}
-
 }
